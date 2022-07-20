d6f2fc61972241ef222491f0df2f2ab303aefb20
Ethan SAFAR <esafar@e1r5p5.clusters.42paris.fr>
2022-07-20 15:04:55 +0200

    implemented test main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,15 +6,24 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 17:19:36 by esafar            #+#    #+#             */
-/*   Updated: 2022/07/20 14:56:26 by esafar           ###   ########.fr       */
+/*   Updated: 2022/07/20 15:04:33 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 
-int	main(void)
+int main()
 {
-    Animal dog();
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
 
-	return (0);
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+
+    return (0);
 }
