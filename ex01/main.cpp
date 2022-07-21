fce0b1a30bbbc6b99390e40528e4f886724faf8a
Ethan SAFAR <esafar@e1r4p5.clusters.42paris.fr>
2022-07-21 17:28:16 +0200

    no memory leaks, memory been free properly

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: esafar <esafar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 17:19:36 by esafar            #+#    #+#             */
-/*   Updated: 2022/07/21 17:16:33 by esafar           ###   ########.fr       */
+/*   Updated: 2022/07/21 17:25:58 by esafar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int main()
 
     // Brain   *brain;
 
+    //malloc each animal of the animal tab
     for (int i = 0; i < NB_ANIMALS; i++)
     {
         if ((i % 2) == 0)
@@ -31,7 +32,7 @@ int main()
         std::cout << "Animal ["<< i << "] has been created with ["<< animal[i]->getType() << "] type." << std::endl;
     }
 
-
+    //free every animal of the tab
     for (int j = 0; j < NB_ANIMALS; j++)
     {
         delete animal[j];
